dd97642100626916f5e60b1876222fdd61050fa4
Bullets-jyr <jyr@bullets.co.kr>
2023-01-30 13:24:44 +0900

    3.4 오늘의 질문 레이아웃 만들기

--- app/src/main/java/kr/co/bullets/dailyq/ui/today/TodayFragment.kt
+++ app/src/main/java/kr/co/bullets/dailyq/ui/today/TodayFragment.kt
@@ -1,7 +1,29 @@
 package kr.co.bullets.dailyq.ui.today
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kr.co.bullets.dailyq.databinding.FragmentTodayBinding
 import kr.co.bullets.dailyq.ui.base.BaseFragment
 
 class TodayFragment : BaseFragment() {
 
+    var _binding: FragmentTodayBinding? = null
+    val binding
+        get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentTodayBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
 }
